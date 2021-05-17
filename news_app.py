1a027de41a5c2d5b2c9b216652597b298586b9fc
ozzutest <150900@student.uwm.edu.pl>
2021-05-17 09:42:02 +0200

    Refactored code for pagination

--- news_app.py
+++ news_app.py
@@ -7,7 +7,9 @@ from flask_nav import Nav
 from flask_nav.elements import Navbar, View
 from flask_paginate import Pagination, get_page_parameter
 
+
 ROWS_PER_PAGE = 10
+
 
 app = Flask(__name__)
 app.config.update(
@@ -15,10 +17,10 @@ app.config.update(
     DEBUG=True,
     APPLICATION_ROOT=os.getcwd()
 )
-
 Bootstrap(app)
 
 
+# Retrieve data from JSON
 def get_json_file(news_type):
     if news_type == 'news':
         filename = os.path.join(app.config.get('APPLICATION_ROOT'),
@@ -33,6 +35,17 @@ def get_json_file(news_type):
     return data
 
 
+# Pagination for retrieved data from JSON
+def paginate(data, page):
+    copy = None
+    switch = {1: data[:ROWS_PER_PAGE], 2: data[12: 12 + ROWS_PER_PAGE],
+              3: data[22: 22 + ROWS_PER_PAGE], 4: data[32: 32 + ROWS_PER_PAGE],
+              5: data[42: 42 + ROWS_PER_PAGE]}
+    copy = switch.get(page, None)
+    return copy
+
+
+# Pick the random articles
 def randomize_data(news):
     return random.choices(news, k=3)
 
@@ -41,6 +54,7 @@ news = get_json_file('news')
 beststories = get_json_file('beststories')
 
 
+# Main page
 @app.route("/", methods=['GET'])
 def main_page_news():
     data = randomize_data(news) + randomize_data(beststories)
@@ -51,32 +65,19 @@ def main_page_news():
 @app.route('/hackernews/news', methods=['GET'])
 def hackernews_news_page():
     data = news
-    copy = None
     search = False
     q = request.args.get('q')
     if q:
         search = True
     page = request.args.get(get_page_parameter(), type=int, default=1)
-
-    if page == 1:
-        copy = data[:ROWS_PER_PAGE]
-    elif page == 2:
-        copy = data[12: 12 + ROWS_PER_PAGE]
-    elif page == 3:
-        copy = data[22: 22 + ROWS_PER_PAGE]
-    elif page == 4:
-        copy = data[32: 32 + ROWS_PER_PAGE]
-    elif page == 5:
-        copy = data[42: 42 + ROWS_PER_PAGE]
-
+    copy = paginate(data, page)
     pagination = Pagination(page=page,
                             per_page=ROWS_PER_PAGE, total=len(data) - 1,
                             search=search, record_name='data', css_framework='foundation')
     return render_template('newstories.html', data=copy, pagination=pagination)
 
+
 # Hacker news - beststories
-
-
 @app.route('/hackernews/beststories', methods=['GET'])
 def hacker_news_beststories_page():
     ...
@@ -87,7 +88,7 @@ nav = Nav()
 
 nav.register_element('top', Navbar(
     View('Home', 'main_page_news'),
-    View('Hacker news', 'hackernews_news_page'),
+    View('Hackernews - newstories', 'hackernews_news_page'),
     View('Hackernews - bestories', 'hacker_news_beststories_page')
 ))
 
